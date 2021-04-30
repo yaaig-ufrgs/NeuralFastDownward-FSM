7ec5aa0a025db4d911fc5eeadc0a931a2d7a2f73
pprobst <pprobst@insiberia.net>
2021-04-30 20:41:50 -0300

    Add tests folder, describe problem

--- tests/blocks/run17.sh
+++ tests/blocks/run17.sh
@@ -9,3 +9,8 @@ PROBLEM="../../tasks/blocksworld_ipc/probBLOCKS-17-0/p$1.pddl"
 ../../fast-downward.py --build debug $PROBLEM \
   --search "eager_greedy([nh(network=snet(type=classification, path=${MODEL}, state_layer=dense_3_input, \
   output_layers=[output_layer/Softmax]))], cost_type=ONE)"
+
+# From src/search/neural_networks/state_network.cc
+# state_layer: "Name of the input layer in the computation graph to insert the current state."
+
+# The error seems to be on the input data. How to discover the name of input layer in the trained model?
