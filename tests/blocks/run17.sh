e3f1dfd140f51aacc013525e16f4e0a80ef7a673
pprobst <pprobst@insiberia.net>
2021-05-01 16:56:40 -0300

    Add working tests; need to explore more

--- tests/blocks/run17.sh
+++ tests/blocks/run17.sh
@@ -2,15 +2,16 @@
 
 # Enter a value between 1 and 200 to select the task to attempt to solve.
 
+# This test in particular takes a ton of time (more than 30 minutes) to solve,
+# because it represents the "hardest" blocksworld problems.
+
 MODEL="17_ocls_ns_ubal_h3_sigmoid_inter_gen_sat_drp0_Kall_pruneOff_9_fold_model.pb"
 DOMAIN="../../tasks/blocksworld_ipc/probBLOCKS-17-0/domain.pddl"
 PROBLEM="../../tasks/blocksworld_ipc/probBLOCKS-17-0/p$1.pddl"
 
-../../fast-downward.py --build debug $PROBLEM \
-  --search "eager_greedy([nh(network=snet(type=classification, path=${MODEL}, state_layer=dense_3_input, \
-  output_layers=[output_layer/Softmax]))], cost_type=ONE)"
+#../../fast-downward.py --build debug $PROBLEM \
+#  --search "eager_greedy([nh(network=sgnet(type=classification, path=${MODEL}, state_layer=input_1_1, \
+#  goal_layer=input_2_1, output_layers=[dense_4_1/Sigmoid]))], cost_type=ONE)"
 
-# From src/search/neural_networks/state_network.cc
-# state_layer: "Name of the input layer in the computation graph to insert the current state."
-
-# The error seems to be on the input data. How to discover the name of input layer in the trained model?
+../../fast-downward.py --overall-time-limit 30m --overall-memory-limit 3584M --build debug $PROBLEM \
+  --search "eager_greedy([nh(network=sgnet(path=$MODEL,type=classification,unary_threshold=0.00001,state_layer=input_1_1,goal_layer=input_2_1,output_layers=[dense_4_1/Sigmoid],atoms={PDDL_ATOMS_FLEXIBLE},defaults={PDDL_INITS_FLEXIBLE},batch_size=50))],cost_type=one)"
