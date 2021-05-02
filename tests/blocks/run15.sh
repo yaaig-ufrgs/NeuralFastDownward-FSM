45c6633b9d006d7e78bf1e7964eebb442c3a25f2
pprobst <pprobst@insiberia.net>
2021-05-01 21:14:27 -0300

    Add one more test and change script to better reflect paper

---
+++ tests/blocks/run15.sh
@@ -0,0 +1,10 @@
+MODEL="15_ocls_ns_ubal_h3_sigmoid_inter_gen_sat_drp0_Kall_pruneOff_9_fold_model.pb"
+DOMAIN="../../tasks/blocksworld_ipc/probBLOCKS-15-0/domain.pddl"
+PROBLEM="../../tasks/blocksworld_ipc/probBLOCKS-15-0/p$1.pddl"
+
+#../../fast-downward.py --build debug $PROBLEM \
+#  --search "eager_greedy([nh(network=sgnet(type=classification, path=${MODEL}, state_layer=input_1_1, \
+#  goal_layer=input_2_1, output_layers=[dense_4_1/Sigmoid]))], cost_type=ONE)"
+
+../../fast-downward.py --overall-time-limit 30m --overall-memory-limit 3584M --build debug $PROBLEM \
+  --search "eager_greedy([nh(network=sgnet(path=$MODEL,type=classification,unary_threshold=0.00001,state_layer=input_1_1,goal_layer=input_2_1,output_layers=[dense_4_1/Sigmoid],atoms={PDDL_ATOMS_FLEXIBLE},defaults={PDDL_INITS_FLEXIBLE},batch_size=50))],cost_type=one)"
