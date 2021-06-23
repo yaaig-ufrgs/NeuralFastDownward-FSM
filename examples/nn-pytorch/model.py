b6a53c917c7592e70bb0dd8501cb495862221cab
pprobst <pprobst@insiberia.net>
2021-06-23 15:52:10 -0300

    Add TODO files

---
+++ examples/nn-pytorch/model.py
@@ -0,0 +1,7 @@
+import torch
+import math
+import sys
+
+# TODO Prepare training data
+# TODO Create model
+# TODO Train
---
+++ examples/nn-pytorch/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../../fast-downward.py --build debug ~/repositories/benchmarks/gripper/prob01.pddl --search "astar(nh(ttest(path=traced.pt)))"
