ac3e63a4a4e4d9388abfc832ddc08c10ff15d8ce
pprobst <pprobst@insiberia.net>
2021-06-25 17:14:59 -0300

    Remove parens from states, TODO states to boolean

--- examples/nn-pytorch/training_data.py
+++ examples/nn-pytorch/training_data.py
@@ -1,8 +1,6 @@
 from json import load
 
-JSON_FILE = "domain_to_training_pairs_blocks.json"
-
-def load_training_data(json_file: str):
+def load_training_state_value_tuples(json_file: str):
     """
     Load state-value pairs from a JSON file (Shen's format),
     returning a list of tuples in the format [([state], value)..].
@@ -13,11 +11,39 @@ def load_training_data(json_file: str):
 
         for domain, training_pairs in data.items():
             for pair in training_pairs:
-                state_value = (pair['state'], pair['value'])
+                state = [e[1:-1] for e in pair['state']]
+                state_value = (state, pair['value'])
                 state_value_pairs.append(state_value)
 
         return state_value_pairs
 
-# This will take time to let's focus on this later.
+"""
+Blocksworld state:
+
+handempty
+clear b1
+on-table b3
+on b1 b2
+on b2 b3
+
+Boolean format example for max blocks = 5
+
+| handempty |   clear   |   holding   |   object  |  on-table |         on          |
+-------------------------------------------------------------------------------------
+| 1         | 1 0 0 0 0 |  0 0 0 0 0  | 0 0 0 0 0 | 0 0 1 0 0 | 1 0 0 1 0 0 0 0 0 0 |
+"""
+def states_to_boolean(data: [], domain: str):
+    bool_states = []
+    states = [t[0] for t in data]
+    print(states)
+
+    if domain == "blocksworld":
+       bool_state = [0]*31 
+    
+
+# TODO - to avoid having to take the state-value pairs from Shen's data.
 def generate_optimal_state_value_pairs(problem):
     pass
+
+#data = load_training_state_value_tuples("domain_to_training_pairs_blocks.json")
+#boolean_states = states_to_boolean(data, "blocksworld")
