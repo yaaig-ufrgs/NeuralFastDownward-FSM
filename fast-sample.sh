8c52e2ea152a19a164e021c9e773b7c55470b956
bettker <rafaelvalesb@gmail.com>
2021-07-22 20:39:11 -0300

    Add blocksworld samples

--- fast-sample.sh
+++ fast-sample.sh
@@ -4,7 +4,7 @@
 #
 # Use: $ ./fast-sample.sh DOMAIN PROBLEM NB_OF_TASKS_TO_GENERATE MIN_WALK_LENGTH MAX_WALK_LENGTH [forward | backward]
 # e.g. $ ./fast-sample.sh domain.pddl p01.pddl 5 10 20
-# e.g. $ ./fast-sample.sh domain.pddl p01.pddl 50 10 20 backward
+#      $ ./fast-sample.sh domain.pddl p01.pddl 50 10 20 backward
 
 DOMAIN=$1
 PROBLEM=$2
@@ -23,7 +23,7 @@ then
 fi
 
 $(dirname $0)/fast-downward.py --build debug $DOMAIN $PROBLEM \
-                   --search "sampling_search_simple(astar(lmcut(transform=sampling_transform()), \
+                   --search "sampling_search_simple(eager_greedy([ff(transform=sampling_transform())], \
                              transform=sampling_transform()), \
                              techniques=[${TECHNIQUE}_none($NB_OF_TASKS_TO_GENERATE, \
                                 distribution=uniform_int_dist($MIN_WALK_LENGTH, $MAX_WALK_LENGTH))])"
