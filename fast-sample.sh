2e757e755a00f3815d93af46222c7927066f19bc
pprobst <pprobst@insiberia.net>
2021-08-02 21:31:29 -0300

    Update fast-sample to use new sampling engine

--- fast-sample.sh
+++ fast-sample.sh
@@ -2,9 +2,9 @@
 #
 # Generate from a given task a new task (e.g. by changing the initial state)
 #
-# Use: $ ./fast-sample.sh DOMAIN PROBLEM NB_OF_TASKS_TO_GENERATE MIN_WALK_LENGTH MAX_WALK_LENGTH [forward | backward]
+# Use: $ ./fast-sample.sh DOMAIN PROBLEM NB_OF_TASKS_TO_GENERATE MIN_WALK_LENGTH MAX_WALK_LENGTH [forward | backward] [random_state | entire_plan | init_state |]
 # e.g. $ ./fast-sample.sh domain.pddl p01.pddl 5 10 20
-#      $ ./fast-sample.sh domain.pddl p01.pddl 50 10 20 backward
+#      $ ./fast-sample.sh domain.pddl p01.pddl 50 10 20 backward entire_plan
 
 DOMAIN=$1
 PROBLEM=$2
@@ -13,9 +13,11 @@ MIN_WALK_LENGTH=$4
 MAX_WALK_LENGTH=$5
 
 TECHNIQUE="iforward" # iforward or gbackward
-if [[ $# == 6 ]];
+SELECT_STATE="random_state"
+if [[ $# == 7 ]];
 then
     TECHNIQUE=$6
+    SELECT_STATE=$7
 
     if [[ $TECHNIQUE == "forward" ]]; then TECHNIQUE="iforward"
     elif [[ $TECHNIQUE == "backward" ]]; then TECHNIQUE="gbackward"
@@ -23,7 +25,8 @@ then
 fi
 
 $(dirname $0)/fast-downward.py --build debug $DOMAIN $PROBLEM \
-                   --search "sampling_search_simple(eager_greedy([ff(transform=sampling_transform())], \
+                   --search "sampling_search_ferber(eager_greedy([ff(transform=sampling_transform())], \
                              transform=sampling_transform()), \
                              techniques=[${TECHNIQUE}_none($NB_OF_TASKS_TO_GENERATE, \
-                                distribution=uniform_int_dist($MIN_WALK_LENGTH, $MAX_WALK_LENGTH))])"
+                                distribution=uniform_int_dist($MIN_WALK_LENGTH, $MAX_WALK_LENGTH))],
+                                select_state_method=$SELECT_STATE)"
