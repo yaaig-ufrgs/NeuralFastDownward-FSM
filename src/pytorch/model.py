582d99a4944e98832aeab64b411910673d1fbcb3
pprobst <pprobst@insiberia.net>
2021-08-03 18:55:34 -0300

    Adjust for logger (WIP; not working)

--- src/pytorch/model.py
+++ src/pytorch/model.py
@@ -1,33 +1,30 @@
 import torch
 import torch.nn as nn
 
-# TODO Create model (Ferber)
-#     - Supervised learning - DONE
-#     - Feed forward - DONE
-#     - Input = vector representation of a state S (boolean) - DONE for blocksworld
-#     - 1-3 hidden layers, Sigmoid activation - DONE
-#     - Output = heuristic estimate of the distance from S to a goal DONE
-#     - Adam optimizer, MSE, Batch size 100 - WAIT
-#     - 10-fold cross-validation (9 as training, 1 as validation) --> TODO low-priority
+# TODO parametrization
 
 # H(euristic) Neural Network
 class HNN(nn.Module):
     def __init__(
         self,
-        input_size: int,
-        hidden_units: int,
-        output_size: int,
+        input_units: int,
+        nb_layers: int,
+        output_units: int,
     ):
         super(HNN, self).__init__()
-        self.hid1 = nn.Linear(input_size, hidden_units)
-        # self.hid2 = nn.Linear(hidden_units, hidden_units)
-        # self.hid3 = nn.Linear(hidden_units, hidden_units)
-        self.opt = nn.Linear(hidden_units, output_size)
+        self.input_units = input_units
+        self.output_units = output_units
+        self.nb_layers = nb_layers
+        unit_diff = input_units - output_units
+        step = int(unit_diff / (nb_layers+1))
+
+        self.hid1 = nn.Linear(input_units-0*step, input_units-1*step)
+        self.opt = nn.Linear(input_units-nb_layers*step, output_units)
+
 
     def forward(self, x):
         z = torch.sigmoid(self.hid1(x))
         # z = torch.sigmoid(self.hid2(z))
-        # z = torch.sigmoid(self.hid3(z))
         z = torch.sigmoid(self.opt(z))
         # z = torch.flatten(self.opt(z))
         return z
