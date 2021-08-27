94cacad8db13c80a2b10d60959c203f4698254e9
bettker <rafaelvalesb@gmail.com>
2021-08-27 19:07:47 -0300

    Move training statistics code

--- src/pytorch/utils/helpers.py
+++ src/pytorch/utils/helpers.py
@@ -17,19 +17,11 @@ def to_onehot(n: int, max_value: int) -> [int]:
     max_value += 1
     return [1 if i == n else 0 for i in range(max_value)]
 
-def get_domain_from_samples_filename(samples):
-    # TODO
-    return ""
-
-def get_problem_from_samples_filename(samples):
-    # TODO
-    return samples.split("/")[-1]
-
 def get_datetime():
     return datetime.now().isoformat().replace('-', '.').replace(':', '.')
 
 def create_train_directory(args, config_in_foldername = False):
-    dirname = get_problem_from_samples_filename(args.samples.name)
+    dirname = args.samples.name.split("/")[-1]
     if config_in_foldername:
         dirname += f"_{args.activation}_{args.output_layer}_" + \
             f"hid{args.hidden_layers}_w{args.weight_decay}_d{args.dropout_rate}"
@@ -58,7 +50,6 @@ def save_json(filename: str, data: list):
 
 def logging_train_config(args, dirname, json=True):
     args_dic = {
-        "domain" : get_domain_from_samples_filename(args.samples.name),
         "samples" : args.samples.name,
         "output_layer" : args.output_layer,
         "num_folds" : args.num_folds,
@@ -83,9 +74,8 @@ def logging_train_config(args, dirname, json=True):
     if json:
         save_json(f"{dirname}/train_args.json", args_dic)
 
-def logging_test_config(args, dirname, json=True):
+def logging_test_config(args, dirname, save_file=True):
     args_dic = {
-        "domain" : get_domain_from_samples_filename(dirname),
         "model" : str(args.model_folder),
         "domain_pddl" : args.domain_pddl,
         "problems_pddl" : args.problem_pddls,
@@ -98,6 +88,54 @@ def logging_test_config(args, dirname, json=True):
     for a in args_dic:
         _log.info(f" | {a}: {args_dic[a]}")
 
-    if json:
+    if save_file:
         save_json(f"{dirname}/test_args.json", args_dic)
 
+def logging_test_statistics(args, dirname, problems_output, save_file=True, decimal_places=4):
+    results = {
+        "configuration" : {
+            "search_algorithm" : args.search_algorithm,
+            "max_search_time" : args.max_search_time,
+            "max_search_memory" : args.max_search_memory
+        },
+        "results" : problems_output,
+        "statistics" : {}
+    }
+
+    rlist = {}
+    for x in results["results"][args.problem_pddls[0]]:
+        rlist[x] = [results["results"][p][x] for p in results["results"] if x in results["results"][p]]
+        if x == "search_state":
+            rlist[x] = [results["results"][p][x] for p in results["results"]]
+            results["statistics"]["plans_found"] = rlist[x].count("success")
+            results["statistics"]["total_problems"] = len(rlist[x])
+            results["statistics"]["coverage"] = \
+                round(
+                    results["statistics"]["plans_found"] / results["statistics"]["total_problems"],
+                    decimal_places
+                )
+        elif x == "plan_length":
+            for i in range(len(rlist[x])):
+                rlist[x][i] = int(rlist[x][i])
+            results["statistics"]["max_plan_length"] = max(rlist[x])
+            results["statistics"]["min_plan_length"] = min(rlist[x])
+            results["statistics"]["avg_plan_length"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
+        elif x == "total_time":
+            for i in range(len(rlist[x])):
+                rlist[x][i] = float(rlist[x][i])
+            results["statistics"]["total_accumulated_time"] = round(sum(rlist[x]), decimal_places)
+        elif x == "search_time":
+            for i in range(len(rlist[x])):
+                rlist[x][i] = float(rlist[x][i])
+            results["statistics"]["avg_search_time"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
+        else:
+            for i in range(len(rlist[x])):
+                rlist[x][i] = int(rlist[x][i])
+            results["statistics"][f"avg_{x}"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
+
+    _log.info("Training statistics")
+    for x in results["statistics"]:
+        _log.info(f" | {x}: {results['statistics'][x]}")
+
+    if save_file:
+        save_json(f"{dirname}/test_results.json", results)
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from src.pytorch.log import setup_full_logging
 from src.pytorch.utils.helpers import (
     logging_test_config,
     create_test_directory,
-    save_json,
+    logging_test_statistics,
 )
 from src.pytorch.utils.parse_args import get_test_args
 
@@ -19,22 +19,13 @@ def test_main(args):
     setup_full_logging(dirname)
     logging_test_config(args, dirname)
 
-    results = {
-        "configuration" : {
-            "search_algorithm" : args.search_algorithm,
-            "max_search_time" : args.max_search_time,
-            "max_search_memory" : args.max_search_memory
-        },
-        "results" : {},
-        "stats" : {}
-    }
-
     model_path = f"{args.model_folder}/traced_best.pt"
     assert path.exists(model_path)
 
+    problems_output = {}
     for i, problem_pddl in enumerate(args.problem_pddls):
         _log.info(f"Solving instance \"{problem_pddl}\" ({i+1}/{len(args.problem_pddls)})")
-        results["results"][problem_pddl] = solve_instance_with_fd_nh(
+        problems_output[problem_pddl] = solve_instance_with_fd_nh(
             domain_pddl=args.domain_pddl,
             problem_pddl=problem_pddl,
             traced_model=model_path,
@@ -43,43 +34,10 @@ def test_main(args):
             time_limit=args.max_search_time,
             memory_limit=args.max_search_memory
         )
-        _log.info(f"Result: {results['results'][problem_pddl]}")
+        _log.info(f"{problems_output[problem_pddl]}")
 
-    # Compute test results statistics
-    decimal_places = 6
-    rlist = {}
-    for x in results["results"][args.problem_pddls[0]]:
-        rlist[x] = [results["results"][p][x] for p in results["results"] if x in results["results"][p]]
-        if x == "search_state":
-            rlist[x] = [results["results"][p][x] for p in results["results"]]
-            results["stats"]["plans_found"] = rlist[x].count("success")
-            results["stats"]["total_problems"] = len(rlist[x])
-            results["stats"]["coverage"] = \
-                round(results["stats"]["plans_found"] / results["stats"]["total_problems"], decimal_places)
-        elif x == "plan_length":
-            for i in range(len(rlist[x])):
-                rlist[x][i] = int(rlist[x][i])
-            results["stats"]["max_plan_length"] = max(rlist[x])
-            results["stats"]["min_plan_length"] = min(rlist[x])
-            results["stats"]["avg_plan_length"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
-        elif x == "total_time":
-            for i in range(len(rlist[x])):
-                rlist[x][i] = float(rlist[x][i])
-            results["stats"]["total_accumulated_time"] = round(sum(rlist[x]), decimal_places)
-        elif x == "search_time":
-            for i in range(len(rlist[x])):
-                rlist[x][i] = float(rlist[x][i])
-            results["stats"]["avg_search_time"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
-        else:
-            for i in range(len(rlist[x])):
-                rlist[x][i] = int(rlist[x][i])
-            results["stats"][f"avg_{x}"] = round(sum(rlist[x]) / len(rlist[x]), decimal_places)
-
+    logging_test_statistics(args, dirname, problems_output)
     _log.info("Test complete!")
-    for x in results["stats"]:
-        _log.info(f" | {x}: {results['stats'][x]}")
-
-    save_json(f"{dirname}/test_results.json", results)
 
 if __name__ == "__main__":
     test_main(get_test_args())
