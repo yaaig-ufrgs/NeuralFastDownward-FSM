84c9fad70901949230f36a735097707ee31b203b
pprobst <pprobst@insiberia.net>
2021-09-01 19:39:57 -0300

    Generate samples for all 5 seeds

--- fast-sample-all.sh
+++ fast-sample-all.sh
@@ -3,10 +3,10 @@
 # Generate sample states from a set of instances.
 #
 # Usage:
-# $ ./fast-sample-all.sh [fukunaga|ferber] [rw|dfs] [fs|ps] searches samples_per_search seed problem_dir output_dir
+# $ ./fast-sample-all.sh [fukunaga|ferber] [rw|dfs] [fs|ps] searches samples_per_search problem_dir output_dir
 #
 # Example:
-# $ ./fast-sample-all.sh fukunaga dfs fs 10 10 1 tasks/IPC/blocks testdir
+# $ ./fast-sample-all.sh fukunaga dfs fs 10 10 tasks/IPC/blocks testdir
 #
 # Beware: don't insert slashes on the directories.
 
@@ -15,9 +15,8 @@ TECHNIQUE=$2
 STATE=$3
 SEARCHES=$4
 SAMPLES_PER_SEARCH=$5
-SEED=$6
-PROBLEM_DIR=$7
-OUTPUT_DIR=$8
+PROBLEM_DIR=$6
+OUTPUT_DIR=$7
 USE_DFS="true"
 USE_FULL_STATE="true"
 
@@ -40,11 +39,13 @@ if [ $METHOD = "fukunaga" ]; then
         if [ $prob_name = "domain" ]; then
             continue
         fi
-        ./fast-downward.py --plan-file $OUTPUT_DIR/${METHOD}_${prob_name}_${TECHNIQUE}_${STATE}_${SEARCHES}x${SAMPLES_PER_SEARCH}_ss${SEED} \
-            --build debug $file \
-            --search "sampling_search_fukunaga(astar(lmcut(transform=sampling_transform()), transform=sampling_transform()), \
-            techniques=[gbackward_fukunaga(searches=$SEARCHES, samples_per_search=$SAMPLES_PER_SEARCH, \
-            use_dfs=$USE_DFS, random_seed=$SEED)], use_full_state=$USE_FULL_STATE)"
+        for seed in {1..5}; do
+            ./fast-downward.py --plan-file $OUTPUT_DIR/${METHOD}_${prob_name}_${TECHNIQUE}_${STATE}_${SEARCHES}x${SAMPLES_PER_SEARCH}_ss${seed} \
+                --build debug $file \
+                --search "sampling_search_fukunaga(astar(lmcut(transform=sampling_transform()), transform=sampling_transform()), \
+                techniques=[gbackward_fukunaga(searches=$SEARCHES, samples_per_search=$SAMPLES_PER_SEARCH, \
+                use_dfs=$USE_DFS, random_seed=$seed)], use_full_state=$USE_FULL_STATE)"
+        done
     done
 else
     echo "TODO FERBER"
