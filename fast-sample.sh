4fca28e7fa49cebea11ef9109596d705ff2e05b0
Rafael Bettker <rafaelvalesb@gmail.com>
2021-09-02 17:12:49 -0300

    Rename tasks and add domain in sample name

--- fast-sample.sh
+++ fast-sample.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 #
-# Generate from a given task a new task (e.g. by changing the initial state)
+# Generate sample states from a set of instances.
 #
-# Use: $ ./fast-sample.sh DOMAIN PROBLEM NB_OF_TASKS_TO_GENERATE MIN_WALK_LENGTH MAX_WALK_LENGTH [forward | backward] [random_state | entire_plan | init_state |]
-# e.g. $ ./fast-sample.sh domain.pddl p01.pddl 5 10 20
-#      $ ./fast-sample.sh domain.pddl p01.pddl 50 10 20 backward entire_plan
+# Usage:
+# $ ./fast-sample.sh [fukunaga|ferber] [rw|dfs] [fs|ps] searches samples_per_search n_seeds problem_dir output_dir
+#
+# Example:
+# $ ./fast-sample.sh fukunaga dfs fs 500 200 5 tasks/IPC/blocks samples
+#
+# Beware: don't insert slashes on the directories.
 
-DOMAIN=$1
-PROBLEM=$2
-NB_OF_TASKS_TO_GENERATE=$3
-MIN_WALK_LENGTH=$4
-MAX_WALK_LENGTH=$5
+METHOD=$1
+TECHNIQUE=$2
+STATE=$3
+SEARCHES=$4
+SAMPLES_PER_SEARCH=$5
+N_SEEDS=$6
+PROBLEM_DIR=$7
+OUTPUT_DIR=$8
+USE_DFS="true"
+USE_FULL_STATE="true"
 
-TECHNIQUE="iforward" # iforward or gbackward
-SELECT_STATE="random_state"
-if [[ $# == 7 ]];
-then
-    TECHNIQUE=$6
-    SELECT_STATE=$7
-
-    if [[ $TECHNIQUE == "forward" ]]; then TECHNIQUE="iforward"
-    elif [[ $TECHNIQUE == "backward" ]]; then TECHNIQUE="gbackward"
-    fi
+if [ $TECHNIQUE = "rw" ]; then
+    USE_DFS="false"
 fi
 
-$(dirname $0)/fast-downward.py --build debug $DOMAIN $PROBLEM \
-                   --search "sampling_search_ferber(eager_greedy([ff(transform=sampling_transform())], \
-                             transform=sampling_transform()), \
-                             techniques=[${TECHNIQUE}_none($NB_OF_TASKS_TO_GENERATE, \
-                                distribution=uniform_int_dist($MIN_WALK_LENGTH, $MAX_WALK_LENGTH))],
-                                select_state_method=$SELECT_STATE)"
+if [ $STATE = "ps" ]; then
+    USE_FULL_STATE="false"
+fi
+
+
+mkdir $OUTPUT_DIR
+
+if [ $METHOD = "fukunaga" ]; then
+    files=($PROBLEM_DIR/*.pddl)
+    domain_name=${PROBLEM_DIR##*/}
+    for file in ${files[@]}; do
+        prob_name=${file#*${PROBLEM_DIR}/};
+        prob_name=${prob_name%%.pddl*}
+        if [ $prob_name != "domain" ]; then
+            for seed in $(seq 1 $N_SEEDS); do
+                ./fast-downward.py --plan-file $OUTPUT_DIR/${METHOD}_${domain_name}_${prob_name}_${TECHNIQUE}_${STATE}_${SEARCHES}x${SAMPLES_PER_SEARCH}_ss${seed} \
+                    --build debug $file \
+                    --search "sampling_search_fukunaga(astar(lmcut(transform=sampling_transform()), transform=sampling_transform()), \
+                    techniques=[gbackward_fukunaga(searches=$SEARCHES, samples_per_search=$SAMPLES_PER_SEARCH, \
+                    use_dfs=$USE_DFS, random_seed=$seed)], use_full_state=$USE_FULL_STATE)"
+            done
+        fi
+    done
+else
+    echo "TODO FERBER"
+fi
--- run_experiment.sh
+++ run_experiment.sh
@@ -4,31 +4,34 @@
 # Requirements: tsp, taskset
 #
 # Usage:
-# $ ./run_experiment.sh [fukunaga|ferber] [500x200|30K|100K] [change_all|fixed_net|fixed_sample] problem seed cores
+# $ ./run_experiment.sh [fukunaga|ferber] [500x200|30K|100K] [change_all|fixed_net|fixed_sample|single] cores
 #
 # Example:
-# $ ./run_experiment.sh fukunaga 500x200 fixed_net blocks 1 10
+# $ ./run_experiment.sh fukunaga 500x200 single 10
 
-# run_general <sample_seed> <net_seed> <rw|dfs> <thread>
+# run_general <sample_seed> <net_seed> <rw|dfs> <thread> <runs>
 run_experiment() {
     sample_seed=$1
     net_seed=$2
     sample_type=$3
     cores=$4
-    files=(../samples/${TECHNIQUE}/${PROBLEM}/${TECHNIQUE}_*_${sample_type}_fs_${SAMPLE_SIZE}_ss${sample_seed})
-    files_len=$((${#files[@]}*10))
+    runs=$5
+    files=(samples/${TECHNIQUE}_*_*_${sample_type}_fs_${SAMPLE_SIZE}_ss${sample_seed})
+    files_len=$((${#files[@]}*${runs}))
     max_per_thread=$((($files_len+$cores-1)/$cores))
     for file in ${files[@]} ; do
-        prob=${file#*${TECHNIQUE}_};
-        prob=${prob%%_*}
-        prob_file=${file#*${PROBLEM}/*}
+        problem_file=${file#*"samples"/*}
+        domain=${problem_file##${TECHNIQUE}_}
+        domain=${domain%%_*}
+        problem=${problem_file##${TECHNIQUE}_${domain}_}
+        problem=${problem%%_*}
         if [ $(($COUNTER%$max_per_thread)) = 0 ]; then
             THREAD_ID=$((THREAD_ID+1))
-            tsp taskset -c ${THREAD_ID} ./train-and-test.sh "-o regression -f 1 -hl 1 -hu 16 -t 99999 -e -1 -a sigmoid -s $net_seed $file" \
-                 "results/nfd_train.${prob_file}.ns${net_seed}/ ../downward-benchmarks/${PROBLEM}/${prob}.pddl -t 99999 -e -1 -a eager_greedy -pt all"
-        else
-            tsp -D $((COUNTER-1)) taskset -c ${THREAD_ID} ./train-and-test.sh "-o regression -f 1 -hl 1 -hu 16 -t 99999 -e -1 -a sigmoid -s $net_seed $file" \
-                 "results/nfd_train.${prob_file}.ns${net_seed}/ ../downward-benchmarks/${PROBLEM}/${prob}.pddl -t 99999 -e -1 -a eager_greedy -pt all"
+            tsp taskset -c ${THREAD_ID} ./train-and-test.sh "-o regression -f 1 -hl 1 -hu 16 -t 99999 -e -1 -a relu -s $net_seed $file" \
+                "results/nfd_train.${problem_file}.ns${net_seed}/ tasks/IPC/${domain}/${problem}.pddl -t 99999 -e -1 -a eager_greedy -pt all"
+       else
+            tsp -D $((COUNTER-1)) taskset -c ${THREAD_ID} "./train-and-test.sh -o regression -f 1 -hl 1 -hu 16 -t 99999 -e -1 -a relu -s $net_seed $file" \
+                "results/nfd_train.${problem_file}.ns${net_seed}/ tasks/IPC/${domain}/${problem}.pddl -t 99999 -e -1 -a eager_greedy -pt all"
         fi
 
         COUNTER=$((COUNTER+1))
@@ -38,9 +41,8 @@ run_experiment() {
 TECHNIQUE=$1
 SAMPLE_SIZE=$2
 EXPERIMENT=$3
-PROBLEM=$4
-SEED=$5 # for fixed seed experiments
-CORES=$6
+CORES=$4
+SEED="1" # for fixed seed experiments
 
 tsp -K
 tsp -S $CORES
@@ -50,43 +52,46 @@ if [ $TECHNIQUE = "fukunaga" ]; then
     THREAD_ID=-1
     COUNTER=0
 
-    if [ $EXPERIMENT = "fixed_net" ]; then
+    if [ $EXPERIMENT = "single" ]; then
         # run_experiment <sample_seed> <net_seed> <sample_type> <thread>
-        run_experiment 1 $SEED "rw" $CORES
-        run_experiment 2 $SEED "rw" $CORES
-        run_experiment 3 $SEED "rw" $CORES
-        run_experiment 4 $SEED "rw" $CORES
-        run_experiment 5 $SEED "rw" $CORES
+        run_experiment 1 1 "dfs" $CORES 1
+    elif [ $EXPERIMENT = "fixed_net" ]; then
+        # TODO: for 1..5, dfs rw
+        run_experiment 1 $SEED "rw" $CORES 10
+        run_experiment 2 $SEED "rw" $CORES 10
+        run_experiment 3 $SEED "rw" $CORES 10
+        run_experiment 4 $SEED "rw" $CORES 10
+        run_experiment 5 $SEED "rw" $CORES 10
 
-        run_experiment 1 $SEED "dfs" $CORES
-        run_experiment 2 $SEED "dfs" $CORES
-        run_experiment 3 $SEED "dfs" $CORES
-        run_experiment 4 $SEED "dfs" $CORES
-        run_experiment 5 $SEED "dfs" $CORES
+        run_experiment 1 $SEED "dfs" $CORES 10
+        run_experiment 2 $SEED "dfs" $CORES 10
+        run_experiment 3 $SEED "dfs" $CORES 10
+        run_experiment 4 $SEED "dfs" $CORES 10
+        run_experiment 5 $SEED "dfs" $CORES 10
     elif [ $EXPERIMENT = "fixed_sample" ]; then
-        run_experiment $SEED 1 "rw" $CORES
-        run_experiment $SEED 2 "rw" $CORES
-        run_experiment $SEED 3 "rw" $CORES
-        run_experiment $SEED 4 "rw" $CORES
-        run_experiment $SEED 5 "rw" $CORES
+        run_experiment $SEED 1 "rw" $CORES 10
+        run_experiment $SEED 2 "rw" $CORES 10
+        run_experiment $SEED 3 "rw" $CORES 10
+        run_experiment $SEED 4 "rw" $CORES 10
+        run_experiment $SEED 5 "rw" $CORES 10
 
-        run_experiment $SEED 1 "dfs" $CORES
-        run_experiment $SEED 2 "dfs" $CORES
-        run_experiment $SEED 3 "dfs" $CORES
-        run_experiment $SEED 4 "dfs" $CORES
-        run_experiment $SEED 5 "dfs" $CORES
+        run_experiment $SEED 1 "dfs" $CORES 10
+        run_experiment $SEED 2 "dfs" $CORES 10
+        run_experiment $SEED 3 "dfs" $CORES 10
+        run_experiment $SEED 4 "dfs" $CORES 10
+        run_experiment $SEED 5 "dfs" $CORES 10
     elif [ $EXPERIMENT = "change_all" ]; then
-        run_experiment 1 1 "rw" $CORES
-        run_experiment 2 2 "rw" $CORES
-        run_experiment 3 3 "rw" $CORES
-        run_experiment 4 4 "rw" $CORES
-        run_experiment 5 5 "rw" $CORES
+        run_experiment 1 1 "rw" $CORES 10
+        run_experiment 2 2 "rw" $CORES 10
+        run_experiment 3 3 "rw" $CORES 10
+        run_experiment 4 4 "rw" $CORES 10
+        run_experiment 5 5 "rw" $CORES 10
 
-        run_experiment 1 1 "dfs" $CORES
-        run_experiment 2 2 "dfs" $CORES
-        run_experiment 3 3 "dfs" $CORES
-        run_experiment 4 4 "dfs" $CORES
-        run_experiment 5 5 "dfs" $CORES
+        run_experiment 1 1 "dfs" $CORES 10
+        run_experiment 2 2 "dfs" $CORES 10
+        run_experiment 3 3 "dfs" $CORES 10
+        run_experiment 4 4 "dfs" $CORES 10
+        run_experiment 5 5 "dfs" $CORES 10
     fi
 else
     echo "TODO FERBER"
