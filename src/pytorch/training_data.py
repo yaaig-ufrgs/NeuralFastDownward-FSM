157c0b2420c9b6c56251b33ec8bd0987f984c2cc
pprobst <pprobst@insiberia.net>
2021-09-12 00:02:31 -0300

    Fix silly mistake

--- src/pytorch/training_data.py
+++ src/pytorch/training_data.py
@@ -59,7 +59,7 @@ def load_training_state_value_pairs(samples_file: str) -> ([([int], int)], int):
     for line in lines:
         if line[0] != "#":
             l = line.split("\n")[0].split(";")
-            state = [int(i) for i in l[1]]
+            state = [int(i) for i in l[1:]]
             state_value_pairs.append((state, int(l[0])))
             if state_value_pairs[-1][1] > domain_max_value:
                 domain_max_value = state_value_pairs[-1][1]
