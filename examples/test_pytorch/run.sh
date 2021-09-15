bca583d2fd0b3ac3520b46402a6d64011bcc6c85
Marcus Ritt <marcus.ritt@inf.ufrgs.br>
2021-09-15 01:35:35 +0000

    Revert.

--- examples/test_pytorch/run.sh
+++ examples/test_pytorch/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-../../fast-downward.py --build debug ~/Home/downward-benchmarks/gripper/prob01.pddl --search "astar(nh(ttest(path=traced.pt)))"
+../../fast-downward.py --build debug ~/repositories/benchmarks/gripper/prob01.pddl --search "astar(nh(ttest(path=traced.pt)))"
 
