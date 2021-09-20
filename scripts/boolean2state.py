5c4bb9ecaa3f5c7e988c3fd059e77b30dcc886b8
Rafael Bettker <rafaelvalesb@gmail.com>
2021-09-20 12:54:34 -0300

    Add boolean2state.py

---
+++ scripts/boolean2state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# usage: ./boolean2state.py "Atom holding(e);Atom on(e, a);Atom..." "0010..."
+
+from sys import argv
+
+if argv[1][-1] == ";":
+    argv[1] = argv[1][:-1]
+if argv[1][8] == "=":
+    argv[1] = argv[1].split("=", 1)[1]
+atoms = argv[1].split(";")
+boolean = argv[2]
+
+assert len(atoms) == len(boolean)
+state = []
+for i in range(len(boolean)):
+    if boolean[i] == "1":
+        state.append(atoms[i])
+
+for s in state:
+    s_ = s.split("Atom ")[1].replace("()","").replace("("," ").replace(",","").replace(")","")
+    print(f"({s_})", end=" ")
+print()
