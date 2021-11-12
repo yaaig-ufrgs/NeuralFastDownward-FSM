d2e40be7da1610143473451c211636496302ea23
pprobst <pprobst@insiberia.net>
2021-11-12 17:41:04 -0300

    Add training tasks actually used

---
+++ tasks/ferber21/training_tasks_used/blocks/run_uniform_generator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RANDOM_SEED_LENGTH=$RANDOM$((RANDOM / 10))
+RANDOM_SEED_WALK=$RANDOM$((RANDOM / 10))
+SOURCE_PDDL="$(dirname $0)/source.pddl"
+$DEEPDOWN/fast-downward.py $SOURCE_PDDL --search "generator(write_statistics=false,techniques=[uniform_none(1,random_seed=$RANDOM_SEED_WALK)])" >/dev/null
+$PDDL_GENERATORS/change_pddl_init_via_sas.py $SOURCE_PDDL sas_plan0
+rm sas_plan0
+rm output.sas
+
