f2b2d42f87b2cd0eeaa5037d6c5a5f06d36e3048
Rafael Bettker <rafaelvalesb@gmail.com>
2022-02-05 18:30:20 -0300

    Update `unique_samples.py`

--- scripts/unique_samples.py
+++ scripts/unique_samples.py
@@ -4,22 +4,14 @@
 Get number of unique samples in each arg file.
 
 Usage: ./unique_samples.py samples_file [samples_file ...]
-  e.g. ./unique_samples.py ../../samples/yaaig/blocks/yaaig_probBLOCKS-12-0_*_fullstate_30K_seed*
+  e.g. ./unique_samples.py ../../samples/yaaig/blocks/yaaig_probBLOCKS-12-0_*
 """
 
 from sys import argv
+from numpy import array, unique
 
 for file in argv[1:]:
-    with open(file,"r") as f:
-        lines = f.readlines()[1:]
-    uniques = []
-    total = 0
-    for line in lines:
-        if line[0] != "#":
-            l = line[:-1].split(";", 1)[1]
-            if l not in uniques:
-                uniques.append(l)
-            total += 1
-    uniques = len(uniques)
-    print(file.split("/")[-1], end=",")
-    print(f"{uniques}/{total} ({(100*uniques/total):.2f}%)")
+    with open(file, "r") as f:
+        samples = [x.strip().split(";")[1] for x in f.readlines() if x and x[0] != "#"]
+        u, t = len(unique(array(samples))), len(samples)
+        print(f"{file},{u},{t} ({(100*u/t):.2f}%)")
