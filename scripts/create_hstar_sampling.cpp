b50df28ae872f0ae477cab13c48b8a1467745888
Rafael Bettker <rafaelvalesb@gmail.com>
2022-03-20 22:28:06 -0300

    Update `create_hstar_sampling.cpp` script

--- scripts/create_hstar_sampling.cpp
+++ scripts/create_hstar_sampling.cpp
@@ -1,10 +1,8 @@
 /**
- * Replace the h-values from a sample file with the h* value.
+ * Create the h* samples.
  * 
  * Usage: g++ -o create_hstar_sampling create_hstar_sampling.cpp
- *        ./create_hstar_sampling fs hstar_100pct_file sample_file
- *        ./create_hstar_sampling fs-nomutex hstar_fs_file sample_file
- *        ./create_hstar_sampling vs hstar_100pct_file sample_file
+ *        ./create_hstar_sampling sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]
  */
 
 #include <iostream>
@@ -36,97 +34,88 @@ vector<pair<int,string>> read_samples(string samples_file) {
 vector<int> str2vec(string s) {
     vector<int> v;
     for (char& b : s)
-        v.push_back((int)b - '0');
+        v.push_back(b == '*' ? -1 : (int)b - '0');
     return v;
 }
 
-void fullstate(char* hstar_file, char* samples_file) {
+int main(int argc, char *argv[]) {
+    if (argc < 4 || argc > 5) {
+        cout << "Usage: ./create_hstar_sampling sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]" << endl;
+        exit(1);
+    }
+
+    string filename_statespace = argv[1];
+    string filename_undefinedchar = argv[2];
+    string filename_complete = argv[3];
+    string filename_completenomutex = argc == 5 ? argv[4] : "";
+
+    string filename_hstar = filename_complete;
+    size_t idx = filename_hstar.find("_fs_");
+    assert(idx != std::string::npos);
+    filename_hstar.replace(filename_hstar.begin()+idx, filename_hstar.begin()+idx+4, "_hstar_");
+    string filename_vs = filename_complete;
+    filename_vs.replace(filename_vs.begin()+idx, filename_vs.begin()+idx+4, "_vs_");
+    string filename_hstarnomutex = filename_completenomutex;
+    idx = filename_hstarnomutex.find("_fs-nomutex_");
+    if (idx != std::string::npos)
+        filename_hstarnomutex.replace(filename_hstarnomutex.begin()+idx, filename_hstarnomutex.begin()+idx+12, "_hstar-nomutex_");
+
+    /* creating h* and h*-nomutex */
+
     // create h* trie
     trie::trie<int> trie;
-    for (pair<int,string>& p : read_samples(hstar_file))
+    vector<pair<int,string>> samples_statespace = read_samples(filename_statespace);
+    for (pair<int,string>& p : samples_statespace)
         trie.insert(str2vec(p.second), p.first);
 
-    // for each sample... replace h^sampling by h*
-    for (pair<int,string>& p : read_samples(samples_file)) {
-
-        int hstar = INT_MAX;
-        for (int& hs: trie.find_all_compatible(str2vec(p.second), true))
-            hstar = min(hstar, hs);
-        
-        if (hstar == INT_MAX) {
-            // Exceptions for blocks 7:
-            // blocks 7 has some non-valid states (all goal states)
-            assert(
-                p.second == "0100000000000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000101000001000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000101000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000100000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0100000000000001000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000100000001000000100010000000000100001000000000010000001000"
-            );
-            hstar = 0;
-            // TODO: to use in other domains, modify to accept sample_file with state_representation=values
-        }
-        assert(hstar != INT_MAX);
-
-        cout << hstar << SEP << p.second << endl;
+    ofstream file_hstar, file_hstarnomutex, file_vs;
+    vector<pair<int,string>> samples_complete, samples_completenomutex;
+    file_hstar.open(filename_hstar);
+    samples_complete = read_samples(filename_complete);
+    int num_samples = samples_complete.size();
+    if (filename_completenomutex != "") {
+        file_hstarnomutex.open(filename_hstarnomutex);
+        samples_completenomutex = read_samples(filename_completenomutex);
+        assert(samples_completenomutex.size() == num_samples);
     }
-}
 
-void fullstate_nomutex(char* hstar_file, char* samples_file) {
-    vector<pair<int,string>> hstar_samples = read_samples(hstar_file);
-    vector<pair<int,string>> nomutex_samples = read_samples(samples_file);
-    assert(hstar_samples.size() == nomutex_samples.size());
-    for (int i = 0; i < hstar_samples.size(); i++)
-        cout << hstar_samples[i].first << SEP << nomutex_samples[i].second << endl;
-}
+    vector<pair<int,string>> samples_undefinedchar = read_samples(filename_undefinedchar);
+    assert(samples_undefinedchar.size() == num_samples);
+    for (int i = 0; i < num_samples; i++) {
+        int hstar = INT_MAX;
+        for (int& hs: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu"))
+            hstar = min(hstar, hs);
+        assert(hstar != INT_MAX);
+        file_hstar << hstar << SEP << samples_complete[i].second << endl;
+        if (filename_completenomutex != "")
+            file_hstarnomutex << hstar << SEP << samples_completenomutex[i].second << endl;
+    }
 
-void validstate(char* hstar_file, char* samples_file) {
-    // create h* trie
-    trie::trie<int> trie;
-    vector<pair<int,string>> hstar_samples = read_samples(hstar_file);
-    for (int i = 0; i < hstar_samples.size(); i++)
-        trie.insert(str2vec(hstar_samples[i].second), i);
+    file_hstar.close();
+    cout << filename_hstar << " DONE." << endl;
+    if (filename_completenomutex != "") {
+        file_hstarnomutex.close();
+        cout << filename_hstarnomutex << " DONE." << endl;
+    }
 
-    // for each sample... replace h^sampling by h*
-    for (pair<int,string>& p : read_samples(samples_file)) {
+    /* creating vs */
+
+    // create h* idx-based trie
+    trie::trie<int> trie_idxbased;
+    for (int i = 0; i < samples_statespace.size(); i++)
+        trie.insert(str2vec(samples_statespace[i].second), i);
+    file_vs.open(filename_vs);
+    for (int i = 0; i < num_samples; i++) {
         pair<int,string> vs = make_pair(INT_MAX, "");
-        for (int& idx: trie.find_all_compatible(str2vec(p.second), true)) {
-            if (vs.first > hstar_samples[idx].first)
-                vs = hstar_samples[idx];
-        }
-        
-        if (vs.first == INT_MAX) {
-            // Exceptions for blocks 7:
-            // blocks 7 has some non-valid states (all goal states)
-            assert(
-                p.second == "0100000000000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000101000001000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000101000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000100000000000000100010000000000100001000000000010000001000" ||
-                p.second == "0100000000000001000000100010000000000100001000000000010000001000" ||
-                p.second == "0000000100000001000000100010000000000100001000000000010000001000"
-            );
-            vs = make_pair(0, "0000000101000001000000100010000000000100001000000000010000001000");
-            // TODO: to use in other domains, modify to accept sample_file with state_representation=values
+        for (int& idx: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu")) {
+            if (vs.first > samples_statespace[idx].first)
+                vs = samples_statespace[idx];
         }
         assert(vs.first != INT_MAX);
-
-        cout << vs.first << SEP << vs.second << endl;
+        file_vs << vs.first << SEP << vs.second << endl;
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (strcmp(argv[1], "fs") == 0)
-        fullstate(argv[2], argv[3]);
-    else if (strcmp(argv[1], "fs-nomutex") == 0)
-        fullstate_nomutex(argv[2], argv[3]);
-    else if (strcmp(argv[1], "vs") == 0)
-        validstate(argv[2], argv[3]);
-    else
-        cout << "Usage: ./create_hstar_sampling fs hstar_100pct_file sample_file"
-             << "       ./create_hstar_sampling fs-nomutex hstar_fs_file sample_file"
-             << "       ./create_hstar_sampling vs hstar_100pct_file sample_file" << endl;
+    file_vs.close();
+    cout << filename_vs << " DONE." << endl;
 
     return 0;
 }
