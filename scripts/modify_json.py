d2664dcb3004cb44572b0829108e6e2dc36ab893
pprobst <pprobst@insiberia.net>
2022-03-22 19:42:40 -0300

    If `+` is the first character of `value`, add to the current `value` instead of replacing it

--- scripts/modify_json.py
+++ scripts/modify_json.py
@@ -12,13 +12,24 @@ from sys import argv
 
 section = argv[1]
 key = argv[2]
-value = int(argv[3]) if argv[3].isdigit() else argv[3]
+
+add_to_value = True if argv[3][0] == '+' else False
+
+if add_to_value:
+    v = argv[3][1:]
+    value = int(v) if v.isdigit() else v
+else:
+    value = int(argv[3]) if argv[3].isdigit() else argv[3]
 
 for json_file in argv[4:]:
     with open(json_file) as jf:
         json_decoded = json.load(jf)
 
-    json_decoded[section][key] = value
+    if add_to_value:
+        json_decoded[section][key] += value
+    else:
+        json_decoded[section][key] = value
+
     print(f"{json_file}[{section}][{key}] = {json_decoded[section][key]}\n")
 
     with open(json_file, 'w') as jf:
