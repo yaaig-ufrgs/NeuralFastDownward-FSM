d82d957627d90c9f6f888aadbe4baa4dbc1bfafd
Rafael Bettker <rafaelvalesb@gmail.com>
2022-03-28 17:28:37 +0000

    Update `create_hstar_sampling.cpp`

--- scripts/create_hstar_sampling.cpp
+++ scripts/create_hstar_sampling.cpp
@@ -1,14 +1,15 @@
 /**
  * Create the h* samples.
- * 
+ *
  * Usage: g++ -o create_hstar_sampling create_hstar_sampling.cpp
- *        ./create_hstar_sampling sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]
+ *        ./create_hstar_sampling output_folder sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]
  */
 
 #include <iostream>
 #include <fstream>
 #include <limits.h>
 #include <string.h>
+#include <unordered_map>
 
 #include "../src/search/trie/trie.h"
 
@@ -39,34 +40,59 @@ vector<int> str2vec(string s) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 4 || argc > 5) {
-        cout << "Usage: ./create_hstar_sampling sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]" << endl;
+    if (argc < 6 || argc > 7) {
+        cout << "Usage: ./create_hstar_sampling use_trie output_folder sample-state_space sample-undefined_char sample-complete [sample-complete_no_mutex]" << endl;
         exit(1);
     }
 
-    string filename_statespace = argv[1];
-    string filename_undefinedchar = argv[2];
-    string filename_complete = argv[3];
-    string filename_completenomutex = argc == 5 ? argv[4] : "";
+    bool use_trie = strcmp(argv[1], "true") == 0;
+    bool allow_undefined_h = false;
+
+    string output_folder = argv[2];
+    system(("mkdir " + output_folder).c_str()); // PLATAFORM DEPENDENT
+
+    string filename_statespace = argv[3];
+    string filename_undefinedchar = argv[4];
+    string filename_complete = argv[5];
+    string filename_completenomutex = argc == 7 ? argv[6] : "";
 
     string filename_hstar = filename_complete;
+    size_t pos_slash = filename_hstar.find_last_of("/");
+    if (pos_slash != string::npos)
+        filename_hstar = filename_hstar.substr(pos_slash+1);
     size_t idx = filename_hstar.find("_fs_");
     assert(idx != std::string::npos);
     filename_hstar.replace(filename_hstar.begin()+idx, filename_hstar.begin()+idx+4, "_hstar_");
+    filename_hstar = output_folder + "/" + filename_hstar;
+
     string filename_vs = filename_complete;
+    pos_slash = filename_vs.find_last_of("/");
+    if (pos_slash != string::npos)
+        filename_vs = filename_vs.substr(pos_slash+1);
     filename_vs.replace(filename_vs.begin()+idx, filename_vs.begin()+idx+4, "_vs_");
+    filename_vs = output_folder + "/" + filename_vs;
+
     string filename_hstarnomutex = filename_completenomutex;
+    pos_slash = filename_hstarnomutex.find_last_of("/");
+    if (pos_slash != string::npos)
+        filename_hstarnomutex = filename_hstarnomutex.substr(pos_slash+1);
     idx = filename_hstarnomutex.find("_fs-nomutex_");
     if (idx != std::string::npos)
         filename_hstarnomutex.replace(filename_hstarnomutex.begin()+idx, filename_hstarnomutex.begin()+idx+12, "_hstar-nomutex_");
+    filename_hstarnomutex = output_folder + "/" + filename_hstarnomutex;
 
     /* creating h* and h*-nomutex */
 
     // create h* trie
     trie::trie<int> trie;
+    unordered_map<string,int> map;
     vector<pair<int,string>> samples_statespace = read_samples(filename_statespace);
-    for (pair<int,string>& p : samples_statespace)
-        trie.insert(str2vec(p.second), p.first);
+    for (pair<int,string>& p : samples_statespace) {
+        if (use_trie)
+            trie.insert(str2vec(p.second), p.first);
+        else
+            map.insert({p.second, p.first}); // map.insert(make_pair<string,int>(p.second, p.first));
+    }
 
     ofstream file_hstar, file_hstarnomutex, file_vs;
     vector<pair<int,string>> samples_complete, samples_completenomutex;
@@ -83,9 +109,21 @@ int main(int argc, char *argv[]) {
     assert(samples_undefinedchar.size() == num_samples);
     for (int i = 0; i < num_samples; i++) {
         int hstar = INT_MAX;
-        for (int& hs: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu"))
-            hstar = min(hstar, hs);
-        assert(hstar != INT_MAX);
+        if (use_trie) {
+            for (int& hs: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu"))
+                hstar = min(hstar, hs);
+        } else {
+            if (map.count(samples_complete[i].second) > 0)
+                hstar = map[samples_complete[i].second];
+        }
+
+        if (allow_undefined_h) {
+            if (hstar == INT_MAX)
+                hstar = -1;
+        } else {
+            assert(hstar != INT_MAX);
+        }
+
         file_hstar << hstar << SEP << samples_complete[i].second << endl;
         if (filename_completenomutex != "")
             file_hstarnomutex << hstar << SEP << samples_completenomutex[i].second << endl;
@@ -99,23 +137,29 @@ int main(int argc, char *argv[]) {
     }
 
     /* creating vs */
-
-    // create h* idx-based trie
-    trie::trie<int> trie_idxbased;
-    for (int i = 0; i < samples_statespace.size(); i++)
-        trie.insert(str2vec(samples_statespace[i].second), i);
-    file_vs.open(filename_vs);
-    for (int i = 0; i < num_samples; i++) {
-        pair<int,string> vs = make_pair(INT_MAX, "");
-        for (int& idx: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu")) {
-            if (vs.first > samples_statespace[idx].first)
-                vs = samples_statespace[idx];
+    if (use_trie) {
+        // create h* idx-based trie
+        trie::trie<int> trie_idxbased;
+        for (int i = 0; i < samples_statespace.size(); i++)
+            trie.insert(str2vec(samples_statespace[i].second), i);
+        file_vs.open(filename_vs);
+        for (int i = 0; i < num_samples; i++) {
+            pair<int,string> vs = make_pair(INT_MAX, "");
+            for (int& idx: trie.find_all_compatible(str2vec(samples_undefinedchar[i].second), "v_vu")) {
+                if (vs.first > samples_statespace[idx].first)
+                    vs = samples_statespace[idx];
+            }
+            if (allow_undefined_h) {
+                if (vs.first == INT_MAX)
+                    vs.first = -1;
+            } else {
+                assert(vs.first != INT_MAX);
+            }
+            file_vs << vs.first << SEP << vs.second << endl;
         }
-        assert(vs.first != INT_MAX);
-        file_vs << vs.first << SEP << vs.second << endl;
+        file_vs.close();
+        cout << filename_vs << " DONE." << endl;
     }
-    file_vs.close();
-    cout << filename_vs << " DONE." << endl;
 
     return 0;
 }
