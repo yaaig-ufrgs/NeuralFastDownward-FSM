54a38e74cc56cc6c11b571f7145d165a31b4a6f5
bettker <rafaelvalesb@gmail.com>
2022-10-16 20:41:51 +0000

    Find fd_root automatically in check_statespace_size.sh

--- paper-experiments/table1_small_statespace-size/check_statespace_size.sh
+++ paper-experiments/table1_small_statespace-size/check_statespace_size.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-fd_root=../..
+abs_path=$(pwd)
+fd_root=(${abs_path//\/NeuralFastDownward/ })
+fd_root="${fd_root[0]}/NeuralFastDownward"
 
 for statespace_file in ${fd_root}/tasks/experiments/statespaces/*_hstar; do
     echo $statespace_file $(cat $statespace_file | wc -l)
