f83ca5928a63d27a73f23894fd851767cb241469
bettker <rafaelvalesb@gmail.com>
2022-10-17 23:24:59 +0000

    Update table 8 in paper-experiments

---
+++ paper-experiments/table8_small_h-fstatespace/gen_column_ff.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 eval_logicbased_heuristics.py ff all | column -t -s,
---
+++ paper-experiments/table8_small_h-fstatespace/gen_column_goalcount.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 eval_logicbased_heuristics.py goalcount all | column -t -s,
