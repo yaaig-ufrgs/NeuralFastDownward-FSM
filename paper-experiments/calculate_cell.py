0d0ed572d16bf97c142a1435e13cf9fccd5bc7d0
bettker <rafaelvalesb@gmail.com>
2022-10-20 17:58:11 +0000

    Update calculate_cell.py

--- paper-experiments/calculate_cell.py
+++ paper-experiments/calculate_cell.py
@@ -3,8 +3,8 @@
 """
 Calculates the average of several seeds (i.e. cell from a paper table)
 
-$ python3 calculate_cell.py expanded table2_small_algorithms-hstar/results/blocks_1pct_bfs_200_hstar/*
-$ python3 calculate_cell.py hvalue table2_small_algorithms-hstar/samples/blocks_1pct_bfs_200_hstar/*
+$ python3 calculate_cell.py expanded table2_small_algorithms-hstar/results/*/*
+$ python3 calculate_cell.py hvalue table2_small_algorithms-hstar/samples/*/*
 """
 
 from sys import argv
@@ -13,8 +13,8 @@ import json
 
 def get_avg_expanded(results):
     values = []
-    for folder in folders:
-        with open(os.path.join(folder, "tests/nfd_test/test_results.json"), "r") as f:
+    for result in results:
+        with open(os.path.join(result, "tests/nfd_test/test_results.json"), "r") as f:
             test_results = json.load(f)
         model = list(test_results["statistics"].keys())[0]
         values.append(test_results["statistics"][model]["avg_expanded"])
@@ -30,20 +30,19 @@ def get_avg_hvalue(samples):
 
 if __name__ == "__main__":
     type = argv[1]
-    folders = argv[2:]
-    experiment = None
-    for folder in folders:
+    exp = {}
+    for folder in argv[2:]:
         e = folder.rsplit("/", 1)[0]
-        if not experiment:
-            experiment = e
-        assert experiment == e, "All results must be from the same experiment to compute their mean"
-
-    if type == "expanded":
-        avg = get_avg_expanded(folders)
-    elif type == "hvalue":
-        avg = get_avg_hvalue(folders)
-    else:
-        raise Exception("invalid type")
-    print("experiment:", experiment)
-    print("n:", len(folders))
-    print("avg:", avg)
+        if e not in exp:
+            exp[e] = []
+        exp[e].append(folder)
+    print("experiment,n,avg")
+    for e in exp:
+        for folder in exp[e]:
+            if type == "expanded":
+                avg = get_avg_expanded(exp[e])
+            elif type == "hvalue":
+                avg = get_avg_hvalue(exp[e])
+            else:
+                raise Exception("invalid type")
+        print(e, len(exp[e]), avg, sep=",")
